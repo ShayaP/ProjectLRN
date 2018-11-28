b5d8eb9181e6167c3a212607a64ec8e2f7c68411
Aaron Rothleder <arothled@ucsd.edu>
2018-11-27 18:01:24 -0800

    login.go

--- actions/login.go
+++ actions/login.go
@@ -4,7 +4,7 @@ import "github.com/gobuffalo/buffalo"
 
 // ProfileHandler is a default handler to serve up
 // a profile page.
-func ProfileHandler(c buffalo.Context) error {
+func LoginHandler(c buffalo.Context) error {
 	c.Set("title", "Login")
 	return c.Render(200, r.HTML("login.html"))
 }
