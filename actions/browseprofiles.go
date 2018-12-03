9a5efdfc267f54775b73789006f768fb125056de
shafeenp <spittal@ucsd.edu>
2018-12-02 19:53:09 -0800

    hi

--- actions/browseprofiles.go
+++ actions/browseprofiles.go
@@ -6,5 +6,6 @@ import "github.com/gobuffalo/buffalo"
 // a home page.
 func BrowseProfilesHandler(c buffalo.Context) error {
         c.Set("title", "Browse")
+        c.Set("results", []int{1,2,3,4,5})
         return c.Render(200, r.HTML("browseprofiles.html"))
     }
