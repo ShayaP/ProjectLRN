7335f0361eeacea4f42e41a5c8c547695093a4a0
shafeenp <spittal@ucsd.edu>
2018-12-08 14:58:57 -0800

    hi

--- migrations/schema.sql
+++ migrations/schema.sql
@@ -1,8 +1,8 @@
-CREATE TABLE "schema_migration" (
+CREATE TABLE IF NOT EXISTS "schema_migration" (
 "version" TEXT NOT NULL
 );
 CREATE UNIQUE INDEX "schema_migration_version_idx" ON "schema_migration" (version);
-CREATE TABLE "users" (
+CREATE TABLE IF NOT EXISTS "users" (
 "id" TEXT PRIMARY KEY,
 "first_name" TEXT NOT NULL,
 "last_name" TEXT NOT NULL,
@@ -19,9 +19,7 @@ CREATE TABLE "users" (
 );
 CREATE UNIQUE INDEX "users_email_idx" ON "users" (email);
 CREATE UNIQUE INDEX "users_google_id_idx" ON "users" (google_id);
-<<<<<<< HEAD
-=======
-CREATE TABLE "requests" (
+CREATE TABLE IF NOT EXISTS "requests" (
 "id" TEXT PRIMARY KEY,
 "status" INTEGER NOT NULL,
 "senderid" char(36) NOT NULL,
@@ -32,7 +30,7 @@ CREATE TABLE "requests" (
 );
 CREATE INDEX "requests_senderid_idx" ON "requests" (senderid);
 CREATE INDEX "requests_receiverid_idx" ON "requests" (receiverid);
-CREATE TABLE "reviews" (
+CREATE TABLE IF NOT EXISTS "reviews" (
 "id" TEXT PRIMARY KEY,
 "rating" INTEGER NOT NULL,
 "description" TEXT NOT NULL,
@@ -41,7 +39,7 @@ CREATE TABLE "reviews" (
 "created_at" DATETIME NOT NULL,
 "updated_at" DATETIME NOT NULL
 );
-CREATE TABLE "courses" (
+CREATE TABLE IF NOT EXISTS "courses" (
 "id" TEXT PRIMARY KEY,
 "name" TEXT NOT NULL,
 "instructor" TEXT NOT NULL,
@@ -49,7 +47,7 @@ CREATE TABLE "courses" (
 "created_at" DATETIME NOT NULL,
 "updated_at" DATETIME NOT NULL
 );
-CREATE TABLE "subjects" (
+CREATE TABLE IF NOT EXISTS "subjects" (
 "id" TEXT PRIMARY KEY,
 "name" INTEGER NOT NULL,
 "prof" INTEGER NOT NULL,
@@ -57,7 +55,7 @@ CREATE TABLE "subjects" (
 "created_at" DATETIME NOT NULL,
 "updated_at" DATETIME NOT NULL
 );
-CREATE TABLE "userinfoes" (
+CREATE TABLE IF NOT EXISTS "userinfoes" (
 "id" TEXT PRIMARY KEY,
 "google_id" TEXT NOT NULL,
 "languages" TEXT NOT NULL,
@@ -70,4 +68,3 @@ CREATE TABLE "userinfoes" (
 FOREIGN KEY (user_id) REFERENCES users (id)
 );
 CREATE UNIQUE INDEX "userinfoes_google_id_idx" ON "userinfoes" (google_id);
->>>>>>> 892717d993841b8a56f12b9e0ecf8d51eec58aae
