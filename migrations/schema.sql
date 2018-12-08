892717d993841b8a56f12b9e0ecf8d51eec58aae
ShayaP <ShayaP@LAPTOP-COHG7SG5.localdomain>
2018-12-08 13:04:41 -0800

    find UI and functionality

--- migrations/schema.sql
+++ migrations/schema.sql
@@ -1,8 +1,8 @@
-CREATE TABLE IF NOT EXISTS "schema_migration" (
+CREATE TABLE "schema_migration" (
 "version" TEXT NOT NULL
 );
 CREATE UNIQUE INDEX "schema_migration_version_idx" ON "schema_migration" (version);
-CREATE TABLE IF NOT EXISTS "users" (
+CREATE TABLE "users" (
 "id" TEXT PRIMARY KEY,
 "first_name" TEXT NOT NULL,
 "last_name" TEXT NOT NULL,
@@ -19,7 +19,7 @@ CREATE TABLE IF NOT EXISTS "users" (
 );
 CREATE UNIQUE INDEX "users_email_idx" ON "users" (email);
 CREATE UNIQUE INDEX "users_google_id_idx" ON "users" (google_id);
-CREATE TABLE IF NOT EXISTS "requests" (
+CREATE TABLE "requests" (
 "id" TEXT PRIMARY KEY,
 "status" INTEGER NOT NULL,
 "senderid" char(36) NOT NULL,
@@ -30,7 +30,7 @@ CREATE TABLE IF NOT EXISTS "requests" (
 );
 CREATE INDEX "requests_senderid_idx" ON "requests" (senderid);
 CREATE INDEX "requests_receiverid_idx" ON "requests" (receiverid);
-CREATE TABLE IF NOT EXISTS "reviews" (
+CREATE TABLE "reviews" (
 "id" TEXT PRIMARY KEY,
 "rating" INTEGER NOT NULL,
 "description" TEXT NOT NULL,
@@ -39,7 +39,7 @@ CREATE TABLE IF NOT EXISTS "reviews" (
 "created_at" DATETIME NOT NULL,
 "updated_at" DATETIME NOT NULL
 );
-CREATE TABLE IF NOT EXISTS "courses" (
+CREATE TABLE "courses" (
 "id" TEXT PRIMARY KEY,
 "name" TEXT NOT NULL,
 "instructor" TEXT NOT NULL,
@@ -47,7 +47,7 @@ CREATE TABLE IF NOT EXISTS "courses" (
 "created_at" DATETIME NOT NULL,
 "updated_at" DATETIME NOT NULL
 );
-CREATE TABLE IF NOT EXISTS "subjects" (
+CREATE TABLE "subjects" (
 "id" TEXT PRIMARY KEY,
 "name" INTEGER NOT NULL,
 "prof" INTEGER NOT NULL,
@@ -55,7 +55,7 @@ CREATE TABLE IF NOT EXISTS "subjects" (
 "created_at" DATETIME NOT NULL,
 "updated_at" DATETIME NOT NULL
 );
-CREATE TABLE IF NOT EXISTS "userinfoes" (
+CREATE TABLE "userinfoes" (
 "id" TEXT PRIMARY KEY,
 "google_id" TEXT NOT NULL,
 "languages" TEXT NOT NULL,
