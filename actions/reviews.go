1c2bde2eeb27293508466d8e819c779abc19cf38
ShayaP <ShayaP@LAPTOP-COHG7SG5.localdomain>
2018-12-08 23:01:42 -0800

    saving stuff

--- actions/reviews.go
+++ actions/reviews.go
@@ -18,18 +18,6 @@ func ReviewHandler(c buffalo.Context) error {
 	return c.Render(200, r.HTML("reviewspage.html"))
 }
 
-func ReviewGetUserRatings(c buffalo.Context) [][]string {
-
-	ratings := [][]string{
-		{"Adele","5", "Pretty Good", "10/23/16"},
-		{"Bobbin","4", "Alright", "8/13/12"},
-		{"Dash","2", "So-so", "7/23/18"},
-		{"Justin","5", "Awesome", "8/23/16"},
-		{"Sophie","1", "Not so good", "11/23/16"},
-	}
-	return ratings
-}
-
 func ReviewGetPastUsers(c buffalo.Context) []*models.User {
 	curr_user := c.Session().Get("user").(*models.User)
 	tx := c.Value("tx").(*pop.Connection)
@@ -55,34 +43,6 @@ func ReviewGetPastUsers(c buffalo.Context) []*models.User {
 		users = append(users, user)
 	}
 	return users
-	//now we have a list of all the request.
-
-
-
-	// user_info, err := models.GetInfoByGID(tx, curr_user.GoogleID)
-	// if err != nil {
-	// 	return nil
-	// }
-	// isTutor := curr_user.IsTutor
-	// list := []string{}
-	// if isTutor == 2 {
-	// 	list = user_info.GetTutees()
-	// } else {
-	// 	list = user_info.GetTutors()
-	// }
-
-	// users := []*models.User{}
-	// for _, Id := range list {
-
-	// 	// check if the useres are tutors and tutees here.
-	// 	u, err := models.GetUserByGID(tx, Id)
-	// 	if err != nil {
-	// 		return nil
-	// 	}
-
-	// 	users = append(users, u)
-	// }
-	// return users
 }
 
 func ReviewGetPastReviews(c buffalo.Context) []models.Review {
