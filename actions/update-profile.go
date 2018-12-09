1f78ccd49373a08967a615d8854875d98bee44c4
shafeenp <spittal@ucsd.edu>
2018-12-08 20:05:33 -0800

    Restricted zip input in update-profile.html

--- actions/update-profile.go
+++ actions/update-profile.go
@@ -127,8 +127,10 @@ func UpdateProfileHandler(c buffalo.Context) error {
 	//c.Set("city", address[1])
 	//c.Set("state", address[2])
 	//c.Set("states", states)
-	c.Set("zip", uinfo.Address)
+	c.Set("zip", UpdateProfileGetSelectedZip(c,uinfo.Address))
 	//c.Set("zip", address[3])
+	// get which zip of the three are the user's zip
+
 	// phone number
 	c.Set("phone", user.PhoneNumber)
 	// email
@@ -146,6 +148,35 @@ func UpdateProfileHandler(c buffalo.Context) error {
 	c.Set("languages2", languages[len(languages)/2:len(languages)])
 	c.Set("checked", mapULtoL)
 	return c.Render(200, r.HTML("update-profile.html"))
+}
+
+//Description:	returns slice of 3 strings which contains
+//				1 string which is selected and the other blanks.
+//				In the result slice, 
+//					UCSD zip is at index 0
+//					SDSU zip is at index 1
+//					USD zip is at index 2
+func UpdateProfileGetSelectedZip(c buffalo.Context, zip string) [3]string {
+	result := [3]string{}
+	zipUcsd := "92093"
+	zipSdsu := "92182"
+	zipUsd := "92110"
+	if zip == zipUcsd {
+		result[0] = "selected"
+	} else {
+		result[0] = ""
+	}
+	if zip == zipSdsu{
+		result[1] = "selected"
+	} else {
+		result[1] = ""
+	}
+	if zip == zipUsd {
+		result[2] = "selected"
+	} else {
+		result[2] = ""
+	}
+	return result
 }
 
 func UpdateProfilePOSTHandler(c buffalo.Context) error {
