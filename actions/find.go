fbe7f6ff98bf54d247d47174ca8b0189de9f665c
Madeline Grubbs <mgrubbs@ucsd.edu>
2018-12-08 18:23:36 -0800

    Plushified find.html for languages and added code to get languages in find.go

--- actions/find.go
+++ actions/find.go
@@ -5,7 +5,9 @@ import "github.com/gobuffalo/buffalo"
 // FindHandler is a default handler to serve up
 // a home page.
 func FindHandler(c buffalo.Context) error {
+	languages := uinfo.GetLanguages()
 	c.Set("title", "Find")
+	c.Set("languages", languages)
 	return c.Render(200, r.HTML("find.html"))
 }
 
