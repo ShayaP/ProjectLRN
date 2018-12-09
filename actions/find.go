097817c5baa6b8fd1db1fb427c8788a87701304c
Madeline Grubbs <mgrubbs@ucsd.edu>
2018-12-08 18:36:24 -0800

    Fix find.go by eliminating imports

--- actions/find.go
+++ actions/find.go
@@ -1,16 +1,9 @@
 package actions
 
-import ( 
-	"github.com/gobuffalo/buffalo"
-	"github.com/gobuffalo/pop"
-	"strings"
-	"github.com/cileonard/lrn/models"
-
-)
 // FindHandler is a default handler to serve up
 // a home page.
 func FindHandler(c buffalo.Context) error {
-	languages := uinfo.GetLanguages()
+	languages := ProfileGetLanguages()
 	c.Set("title", "Find")
 	c.Set("languages", languages)
 	return c.Render(200, r.HTML("find.html"))
